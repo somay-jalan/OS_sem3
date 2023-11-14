c3f75832e8b6efbc156431eb0d51371b3413ceeb
Somay jalan <jalansomay0@gmail.com>
2023-11-14 14:26:23 +0530

    Adding code for q2 and correcting code for q1

--- assignment4/assignment/question1/dining_philos.c
+++ assignment4/assignment/question1/dining_philos.c
@@ -4,6 +4,9 @@
 #include<unistd.h>
 #include<assert.h>
 
+#define sleeptime 1
+#define no_of_loops 10
+ 
 pthread_mutex_t fork1,fork2,fork3,fork4,fork5;
 
 void eating(long philos_num){
@@ -11,35 +14,35 @@ void eating(long philos_num){
     if(philos_num==1){
         pthread_mutex_lock(&fork1);
         pthread_mutex_lock(&fork5);
-        sleep(2);
+        sleep(sleeptime);
         pthread_mutex_unlock(&fork1);
         pthread_mutex_unlock(&fork5);
 
     }else if(philos_num==2){
         pthread_mutex_lock(&fork2);
         pthread_mutex_lock(&fork1);
-        sleep(2);
+        sleep(sleeptime);
         pthread_mutex_unlock(&fork2);
         pthread_mutex_unlock(&fork1);
 
     }else if(philos_num==3){
         pthread_mutex_lock(&fork3);
         pthread_mutex_lock(&fork2);
-        sleep(2);
+        sleep(sleeptime);
         pthread_mutex_unlock(&fork3);
         pthread_mutex_unlock(&fork2);
 
     }else if(philos_num==4){
         pthread_mutex_lock(&fork4);
         pthread_mutex_lock(&fork3);
-        sleep(2);
+        sleep(sleeptime);
         pthread_mutex_unlock(&fork4);
         pthread_mutex_unlock(&fork3);
 
     }else if(philos_num==5){
         pthread_mutex_lock(&fork4);
         pthread_mutex_lock(&fork5);
-        sleep(2);
+        sleep(sleeptime);
         pthread_mutex_unlock(&fork4);
         pthread_mutex_unlock(&fork5);
     }else{
@@ -49,7 +52,7 @@ void eating(long philos_num){
 
 void thinking(long philos_num){
     printf("philospher number:%ld is thinking\n",philos_num);
-    sleep(2);
+    sleep(sleeptime);
 }
 
 void *philospher(void *args){
@@ -60,8 +63,9 @@ void *philospher(void *args){
 }
 
 int main(int argc, char *argv[]){
-    for(int i=0;i<5;i++){
+    for(int i=0;i<no_of_loops;i++){
         printf("------------------------RUNNING ANOTHER LOOP-----------------\n");
+        printf("LOOP NUMBER:%d\n",i+1);
         pthread_t phil1,phil2,phil3,phil4,phil5;
         int rc;
         rc=pthread_create(&phil1,NULL,philospher,(void*)1);assert(rc==0);
---
+++ assignment4/assignment/question2/car.c
@@ -0,0 +1,110 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<pthread.h>
+#include<unistd.h>
+#include<assert.h>
+#include <semaphore.h> 
+
+sem_t load_unload,car_go,boarding_unboarding,car_stop,car_loaded,car_unloaded; 
+int car_load_int,car_unload_int;
+int num_passengers, capacity;
+
+void load(){
+    printf("Loading the car\n");
+    sleep(2);
+}
+
+void unload(){
+    printf("Unloading the car\n");
+    sleep(2);
+}
+
+void board(){
+    printf("Boarding the car\n");
+    sleep(1);
+}
+void unboard(){
+    printf("Unbarding the car\n");
+    sleep(1);
+}
+void *passenger(void *args){
+    long passenger_num=(long)args;
+    sem_wait(&boarding_unboarding);
+    printf("Passenger number :%ld is boarding\n",passenger_num);
+    board();
+    car_load_int++;
+    printf("Passenger number :%ld has boarded\n",passenger_num);
+    sem_post(&boarding_unboarding);
+    if(car_load_int==capacity){
+        sem_post(&car_loaded);
+    }
+    sem_wait(&car_stop);
+    sem_wait(&load_unload);
+    sem_wait(&boarding_unboarding);
+    printf("Passenger number :%ld is unboarding\n",passenger_num);
+    unboard();
+    car_unload_int++;
+    printf("Passenger number :%ld has unboarded\n",passenger_num);
+    sem_post(&boarding_unboarding);
+    sem_post(&car_stop);
+    sem_post(&load_unload);
+    if(car_unload_int==capacity){
+        sem_post(&car_unloaded);
+    }
+}
+
+void *car(void *args){
+    printf("Car thread starting.\n");
+    printf("Car loading\n");
+    load();
+    printf("Car loaded\n");
+    sem_post(&load_unload);
+    sem_wait(&car_go);
+    printf("Car going\n");
+    sleep(2);
+    printf("Car completed its journey\n");
+    sem_post(&car_go);
+    sem_post(&car_stop);
+    printf("Car unloading\n");
+    unload();
+    printf("Car unloaded\n");
+    sem_post(&load_unload);
+    sem_wait(&car_unloaded);
+}
+
+
+
+
+int main(int argc, char *argv[]){
+    pthread_t car_thread,passenger_thread;
+    sem_init(&load_unload,0,0);
+    sem_init(&car_go,0,0);
+    sem_init(&car_stop,0,0);
+    sem_init(&boarding_unboarding,0,1);
+    sem_init(&car_unloaded,0,0);
+    sem_init(&car_loaded,0,0);
+
+    printf("Enter the number of passengers:");
+    scanf("%d",&num_passengers);
+    printf("Enter the capacity of car:");
+    scanf("%d",&capacity);
+    if(capacity>num_passengers){
+        printf("Capacity more than numbers of passengers.\n");
+        return 0;
+    }else if(capacity<num_passengers){
+        printf("There is not enough space in the car first %d passengers will be taken.\n",capacity);
+    }
+    int rc;
+    rc=pthread_create(&car_thread,NULL,car,NULL);assert(rc==0);
+    sem_wait(&load_unload);
+    printf("Boarding passengers\n");
+    for(int i=0;i<capacity;i++){
+        rc=pthread_create(&passenger_thread,NULL,passenger,(void*)(long)(i+1));assert(rc==0);
+    }
+    sem_wait(&car_loaded);
+    printf("Boarding complete\n");
+    sem_post(&car_go);
+    rc=pthread_join(car_thread,NULL);assert(rc==0);
+    printf("All the journey has been done\n");
+    return 0;
+}
